d2c050909b025a941d758bbec167aa551a829745
Livia <liviaalencar60@gmail.com>
2023-09-18 19:45:23 -0300

    CSS

--- main.js
+++ main.js
@@ -1,40 +1,39 @@
 const pesquisarCEP = document.getElementById("bPesquisar");
+pesquisarCEP.addEventListener("click", event =>{
+  event.PreventDefault();
+});
+
 const inputDoCep = document.querySelector("#cep");
 const valorDoCep = inputDoCep.value;
 const url = `https://viacep.com.br/ws/${valorDoCep}/json/`;
 
 fetch(url).then(response =>{
-    return response.json();
-      }).then(data =>
-      {
-        function atribuirCampos(data)
-        {
-        const rua = document.querySelector("#rua");
-        const complemento = document.querySelector("#complemento");
-        const bairro = document.querySelector("#bairro");
-        const cidade = document.querySelector("#cidade");
-        const estado = document.querySelector("#estado");
-        
-        rua.value = data.logradouro;
-        complemento.value = data.complemento;
-        bairro.value = data.bairro;
-        cidade.value = data.localidade;
-        estado.value = data.uf;
-        }
-  })
-
-pesquisarCEP.addEventListener("click", event =>{
-    event.PreventDefault();
-    fetch(url).then(response =>{
-        return response.json();
-            })
- .then(data =>
-    {
-    if(data.erro)
-    {
-    alert("CEP inválido!");
-    return ;
-    }
-    atribuirCampos(data);
-   })
+return response.json();
+    })
+.then(data =>{
+      atribuirCampos(data);
 })
+.then(data =>
+{
+if(data.erro)
+{
+alert("O CEP DIGITADO ESTÁ INVÁLIDO");
+return ;
+}
+atribuirCampos(data);
+})
+
+function atribuirCampos(data)
+{
+const logradouro = document.querySelector("#logradouro");
+const complemento = document.querySelector("#complemento");
+const bairro = document.querySelector("#bairro");
+const localidade = document.querySelector("#localidade");
+const uf = document.querySelector("#uf");
+
+logradouro.value = data.logradouro;
+complemento.value = data.complemento;
+bairro.value = data.bairro;
+localidade.value = data.localidade;
+uf.value = data.uf;
+};
